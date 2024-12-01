2e41cafc31f7bd327049719151d281756549818d
SplenectomY <skaterjohn16@hotmail.com>
2024-12-01 16:04:26 -0500

    Initial commit

---
+++ Day.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    internal abstract class Day
+    {
+        public abstract int DayNumber { get; }
+        public abstract string Solve(string input);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode2024;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+List<Day> days = [];
+
+var assembly = Assembly.GetExecutingAssembly()
+    ?? throw new Exception("Assembly was null");
+
+foreach (var type in assembly.GetTypes())
+{
+    Debug.WriteLine($"AdventofCode Day initialized: {type.Name}");
+
+    if (type.IsSubclassOf(typeof(Day)))
+    {
+        object instance = Activator.CreateInstance(type)
+            ?? throw new Exception("Instance was null");
+
+        days.Add((Day)instance);
+    }
+}
+
+days = [.. days.OrderBy(d => d.DayNumber)];
+
+int? dayNumber = null;
+
+while (dayNumber == null)
+{
+    Console.WriteLine($"Please enter a day number (1 - {days.Count}):");
+    string? dayNumberInput = Console.ReadLine();
+
+    if (int.TryParse(dayNumberInput, out int validInt) && validInt > 0 
+        && validInt <= days.Count)
+    {
+        dayNumber = validInt - 1;
+        break;
+    }
+
+    Console.WriteLine("Invalid integer detected");
+}
+string? input = null;
+
+while (input == null)
+{
+    Console.WriteLine($"Please paste your puzzle input and press enter:");
+    input = Console.ReadLine();
+}
+
+Console.WriteLine(days[(int)dayNumber].Solve(input));
