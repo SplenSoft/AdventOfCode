78fb3aa3fd61eda36cba5a208cb6e9868dfceaac
SplenectomY <skaterjohn16@hotmail.com>
2024-12-01 22:19:09 -0500

    Update Day2.cs
    Solve 2015 day 2

--- 2015/Day2.cs
+++ 2015/Day2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode._2015;
@@ -31,6 +32,41 @@ All numbers in the elves' list are in feet. How many total square feet of wrappi
 
     public override string Solve(string input)
     {
-        throw new NotImplementedException();
+        var regex = new Regex(@"(\d+?)x(\d+?)x(\d+?)(?:\n|\r\n|$)");
+        List<(int, int, int)> list = [];
+
+        foreach (Match match in regex.Matches(input))
+            list.Add((int.Parse(match.Groups[1].Value), 
+                int.Parse(match.Groups[2].Value), 
+                int.Parse(match.Groups[3].Value)));
+
+        int paper = 0;
+        int ribbon = 0;
+
+        foreach (var present in list)
+        {
+            int dim1 = present.Item1 * present.Item2;
+            int dim2 = present.Item2 * present.Item3;
+            int dim3 = present.Item3 * present.Item1;
+
+            int perim1 = (present.Item1 * 2) + (present.Item2 * 2);
+            int perim2 = (present.Item2 * 2) + (present.Item3 * 2);
+            int perim3 = (present.Item3 * 2) + (present.Item1 * 2);
+
+            int surfaceArea = (dim1 * 2) + (dim2 * 2) + (dim3 * 2);
+
+            int smallestDim = dim1 <= dim2 && dim1 <= dim3 ? dim1 
+                : dim2 <= dim1 && dim2 <= dim3 ? dim2 : dim3;
+
+            int ribbon1 = perim1 <= perim2 && perim1 <= perim3 ? perim1 
+                : perim2 <= perim1 && perim2 <= perim3 ? perim2 : perim3;
+
+            int volume = present.Item1 * present.Item2 * present.Item3;
+
+            paper += smallestDim + surfaceArea;
+            ribbon += volume + ribbon1;
+        }
+
+        return $"Part 1 solution: {paper}\nPart 2 solution: {ribbon}";
     }
 }
