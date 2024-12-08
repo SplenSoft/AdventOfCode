3d98d8bf695e32107a619452c947de025abeab20
SplenectomY <skaterjohn16@hotmail.com>
2024-12-08 15:13:56 -0500

    Update Day8.cs
    Condense more

--- 2024/Day8.cs
+++ 2024/Day8.cs
@@ -20,12 +20,10 @@ internal class Day8 : Day
         ants.ForEach(a1 => ants
             .Where(a2 => a1 != a2 && a1.c == a2.c).ToList().ForEach(a2 =>
             {
-                var antiNode = a1.v + (a1.v - a2.v);
-                nodes[0].Add(antiNode);
-                nodes[1] = [.. nodes[1], antiNode, a1.v];
-
-                while (IsInBounds(antiNode))
-                    nodes[1].Add(antiNode += a1.v - a2.v);
+                var node = a1.v + (a1.v - a2.v);
+                nodes[0].Add(node);
+                nodes[1] = [.. nodes[1], node, a1.v];
+                while (IsInBounds(node)) nodes[1].Add(node += a1.v - a2.v);
             }));
 
         bool IsInBounds(Vector2 pos)=> pos.X >= 0 && pos.X < lines[0].Length 
