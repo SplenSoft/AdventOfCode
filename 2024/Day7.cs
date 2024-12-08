c83ea086006060796a0a6d43ec3c55bf852e7d0b
SplenectomY <skaterjohn16@hotmail.com>
2024-12-08 11:01:05 -0500

    Update Day7.cs
    More optimizations

--- 2024/Day7.cs
+++ 2024/Day7.cs
@@ -14,24 +14,23 @@ internal class Day7 : Day
 
     public override async Task<string> Solve(string input)
     {
-        bool Solve(long res, long[] ints, int o, int t)
+        bool Solve(long res, long[] n, int o /*op*/, int t /*total # ops*/)
         {
-            if (ints[0] > res) return false;
-            if (ints.Length == 1) return ints[0] == res;
+            if (n.Length == 1) return n[0] == res;
 
-            long n1 = o == 0 ? ints[0] + ints[1] : o == 1 ? ints[0] * ints[1] 
-                : long.Parse(ints[0].ToString() + ints[1].ToString());
+            long n1 = o == 0 ? n[0] + n[1] : o == 1 ? n[0] * n[1] // +, *
+                : long.Parse(n[0].ToString() + n[1].ToString()); // ||
 
-            for (int i = 0; i < t; i++)
-                if (Solve(res, [..new long[1] { n1 }, ..ints.Skip(2)], i, t))
-                    return true;
+            for (int i = 0; i < t; i++) // Combine n1 + n2 as we go
+                if (Solve(res, [..new long[1] { n1 }, ..n.Skip(2)], i, t))
+                    return true; // We can match res
 
-            return false;
+            return false; // We can't match res
         }
 
-        long[] totals = [0, 0];
+        long[] totals = [0 /*part1*/, 0 /*part2*/];
         string[] lines = input.Split(Environment.NewLine);
-        for (int totalOps = 2; totalOps < 4; totalOps++)
+        for (int totalOps = 2; totalOps < 4; totalOps++) // + and *, then ||
             for (int i = 0; i < lines.Length; i++)
             {
                 var g = new Regex(@"(\d+?):\s(.+)").Match(lines[i]).Groups;
