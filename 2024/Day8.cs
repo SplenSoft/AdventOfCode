7caa4df90d550a6ce8480e523d7b4f9ee99e564e
SplenectomY <skaterjohn16@hotmail.com>
2024-12-08 11:01:11 -0500

    Create Day8.cs

---
+++ 2024/Day8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2024;
+
+internal class Day8 : Day
+{
+    public override int Year => 2024;
+
+    public override int DayNumber => 8;
+
+    public override string Synopsis => throw new NotImplementedException();
+
+    public override string Input => throw new NotImplementedException();
+
+    public override Task<string> Solve(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
