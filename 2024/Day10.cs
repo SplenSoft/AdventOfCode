5ddd4a429471f9b47ec12679fea7887e0eb89377
SplenectomY <skaterjohn16@hotmail.com>
2024-12-10 16:00:36 -0500

    Update Day10.cs
    Solve 2024 day 10

--- 2024/Day10.cs
+++ 2024/Day10.cs
@@ -10,48 +10,43 @@ internal class Day10 : Day
 {
     public override string Input => Resources._2024_10_Input;
     //public override string Input => Resources._2024_10_Test_Input;
-
+    //51
     public override async Task Solve(string input, long[] tot)
     {
         string[] lines = input.Split(Environment.NewLine);
-
         int[,] map = new int[lines[0].Length, lines.Length];
 
         for (int y = 0; y < lines.Length; y++)
             for (int x = 0; x < lines[y].Length; x++)
                 map[x, y] = int.Parse(lines[y][x].ToString());
 
-        bool IsOffMap(Vector2 pos)
-        {
-            return pos.X < 0 || pos.Y < 0 || pos.X >= lines[0].Length || pos.Y >= lines.Length;
-        }
+        for (int y = 0; y < lines.Length; y++)
+            for (int x = 0; x < lines[y].Length; x++)
+                if (map[x, y] == 0)
+                {
+                    List<Vector2> tops = [];
+                    Path([new Vector2(x, y)], tops);
+                    tot[0] += tops.Distinct().Count();
+                    tot[1] += tops.Count;
+                }
 
-        bool IsTopHeight(Vector2 pos)
-        {
-            return map[(int)pos.X, (int)pos.Y] == 9;
-        }
-
-        void Path(List<Vector2> pathSoFar)
+        void Path(List<Vector2> pathSoFar, List<Vector2> tops)
         {
             Vector2 pos = pathSoFar.Last();
             
-            if (IsTopHeight(pos))
-            {
-                tot[0]++;
-                return;
-            }
-            int height = map[(int)pos.X, (int)pos.Y];
-            for (int x = -1; x <= 1; x += 2)
-                for (int y = -1; y <= 1; y += 2)
-                {
-                    Vector2 newPos = new(pos.X + x, pos.Y + y);
-                    int newHeight = map[(int)newPos.X, (int)newPos.Y];
-                    if (newHeight - height != 1) continue;
-                    if (pathSoFar.Contains(newPos)) continue;
-                    if (IsOffMap(newPos)) continue;
-                    List<Vector2> newPath = [..pathSoFar, newPos];
-                    Path(newPath);
-                }
+            if (map[(int)pos.X, (int)pos.Y] == 9) tops.Add(pos);
+            else for (int x = -1; x <= 1; x++)
+                for (int y = -1; y <= 1; y++)
+                    {
+                        Vector2 newPos = new(pos.X + x, pos.Y + y);
+                        if ((x == 0 || y == 0) && newPos.X >= 0
+                            && newPos.X < lines[0].Length
+                            && newPos.Y < lines.Length && newPos.Y >= 0
+                            && map[(int)newPos.X, (int)newPos.Y]
+                            - map[(int)pos.X, (int)pos.Y] == 1
+                            && !pathSoFar.Contains(newPos))
+                                Path([.. pathSoFar, newPos], tops);
+                    }
         }
     }
 }
