2a2db08dedb3b55c6f9d06f38da5099d2d0bbae5
SplenectomY <skaterjohn16@hotmail.com>
2024-12-10 10:02:32 -0500

    Update Day10.cs

--- 2024/Day10.cs
+++ 2024/Day10.cs
@@ -1,1 +1,17 @@
+using System.Numerics;
 
+namespace AdventOfCode._2024;
+
+/// <summary>
+/// <see href="https://adventofcode.com/2024/day/10"/>
+/// </summary>
+[Day(2024, 10)]
+internal class Day10 : Day
+{
+    public override string Input => Resources._2024_10_Input;
+    //public override string Input => Resources._2024_10_Test_Input;
+
+    public override async Task Solve(string input, long[] tot)
+    {
+    }
+}
