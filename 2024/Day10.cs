9e6adcb008881bb93c66992e3bbb0de06625a813
SplenectomY <skaterjohn16@hotmail.com>
2024-12-10 10:07:46 -0500

    Update Day10.cs

--- 2024/Day10.cs
+++ 2024/Day10.cs
@@ -13,5 +13,14 @@ internal class Day10 : Day
 
     public override async Task Solve(string input, long[] tot)
     {
+        string[] lines = input.Split(Environment.NewLine);
+
+        int[,] map = new int[lines[0].Length, lines.Length];
+
+        for (int y = 0; y < lines.Length; y++)
+            for (int x = 0; x < lines[y].Length; x++)
+                map[x][y] = int.Parse(lines[y][x]);
+
+        
     }
 }
