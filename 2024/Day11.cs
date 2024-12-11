46f7be0da01ba5a25799f413ca4eb5c2aab777a2
SplenectomY <skaterjohn16@hotmail.com>
2024-12-11 11:34:07 -0500

    Update Day11.cs

--- 2024/Day11.cs
+++ 2024/Day11.cs
@@ -12,8 +12,32 @@ namespace AdventOfCode._2024;
 [Day(2024, 11)]
 internal class Day11 : Day
 {
-    public override Task Solve(string input, long[] totals)
+    public override async Task Solve(string input, long[] totals)
     {
-        
+        List<long> stones = [.. input.Split(' ').Select(long.Parse)];
+        for (int i = 0; i < 75; i++)
+        {
+            Console.WriteLine($"Blink {i + 1} of {75}");
+            for (int j = 0; j < stones.Count; j++)
+            {
+                long stone = stones[j];
+                if (stone == 0) stones[j] = 1;
+                else if (stone.ToString().Length % 2 == 0)
+                {
+                    int halfIndex = (stone.ToString().Length / 2);
+                    int remainder = stone.ToString().Length - halfIndex;
+                    stones[j] = long.Parse(stone.ToString()[..halfIndex]);
+                    stones.Insert(j + 1, long.Parse(stone.ToString()[^remainder..]));
+                    j++;
+                }
+                else
+                {
+                    stones[j] = stones[j] * 2024;
+                }
+            }
+
+            if (i == 24) totals[0] = stones.Count;
+            if (i == 75) totals[1] = stones.Count;
+        }
     }
 }
