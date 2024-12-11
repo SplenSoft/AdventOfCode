20c46ca39045f309468a776c60172fec3622b640
SplenectomY <skaterjohn16@hotmail.com>
2024-12-11 12:36:05 -0500

    Update Day11.cs
    Parallel ... memory issues now

--- 2024/Day11.cs
+++ 2024/Day11.cs
@@ -19,40 +19,50 @@ internal class Day11 : Day
         ConcurrentBag<long> newStones = [];
         for (int i = 0; i < 75; i++)
         {
-            Console.WriteLine($"Blink {i + 1} of {75}");
+            
             newStones.Clear();
 
             long stonesPerProcessor = (long)Math.Ceiling((decimal)stones.Count / Environment.ProcessorCount);
-
+            Console.WriteLine($"Blink {i + 1} of {75}. Stones per processor = {stonesPerProcessor}");
             List<Action> tasks = [];
 
-            for (int j = 0; j < stonesPerProcessor; j++) 
-            { 
-                
-            }
-
-            Parallel.ForEach(stones, stone =>
+            for (int k = 0; k < Environment.ProcessorCount; k++) 
             {
-                string asString = stone.ToString();
-                if (stone == 0)
+                long max = Math.Min(stonesPerProcessor * (k + 1), stones.Count);
+                int iterator = k;
+                tasks.Add(() => 
                 {
-                    newStones.Add(1);
-                }
-                else if (asString.Length % 2 == 0)
-                {
-                    int halfIndex = (asString.Length / 2);
-                    int remainder = asString.Length - halfIndex;
-                    newStones.Add(long.Parse(asString[..halfIndex]));
-                    newStones.Add(long.Parse(asString[^remainder..]));
-                }
-                else
-                {
-                    newStones.Add(stone * 2024);
-                }
-            });
+                    
+                    Console.WriteLine($"Task {iterator + 1} in progress, max is {max}, starting {stonesPerProcessor * iterator}");
+                    for (long j = stonesPerProcessor * iterator; j < max; j++)
+                    {
+                        //Console.WriteLine($"j == {j}");
+                        long stone = stones[(int)j];
+                        string asString = stone.ToString();
+                        if (stone == 0)
+                        {
+                            newStones.Add(1);
+                        }
+                        else if (asString.Length % 2 == 0)
+                        {
+                            int halfIndex = asString.Length / 2;
+                            int remainder = asString.Length - halfIndex;
+                            newStones.Add(long.Parse(asString[..halfIndex]));
+                            newStones.Add(long.Parse(asString[^remainder..]));
+                        }
+                        else
+                        {
+                            newStones.Add(stone * 2024);
+                        }
+                    }
+                    Console.WriteLine($"Stone subtotal: {newStones.Count}");
+                });
+            }
+            Parallel.Invoke(tasks.ToArray());
+            Console.WriteLine($"Stone total: {newStones.Count}");
             stones = [..newStones];
             if (i == 24) totals[0] = stones.Count;
-            if (i == 75) totals[1] = stones.Count;
+            if (i == 74) totals[1] = stones.Count;
         }
     }
 }
