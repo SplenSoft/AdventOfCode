cd6f4f9f0a313f80274e7d595af5c7dd0dc9d59a
SplenectomY <skaterjohn16@hotmail.com>
2024-12-12 11:54:00 -0500

    Create Day12.cs

---
+++ 2024/Day12.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2024;
+
+internal class Day12 : Day
+{
+    public override Task Solve(string input, long[] totals)
+    {
+        
+    }
+}
