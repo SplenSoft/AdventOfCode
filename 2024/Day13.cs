321f00de8ef3a8fd8b51ae0b27039462879641de
SplenectomY <skaterjohn16@hotmail.com>
2024-12-13 22:26:33 -0500

    Update Day13.cs
    Solve part 2 and condense

--- 2024/Day13.cs
+++ 2024/Day13.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
+﻿using System.Numerics;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace AdventOfCode._2024;
 
@@ -22,58 +17,24 @@ internal class Day13 : Day
 
         Vector2 Match(Regex regex, string line)
         {
-            var match = regex.Match(line).Groups;
-            return new (int.Parse(match[1].Value), int.Parse(match[2].Value));
+            var g = regex.Match(line).Groups;
+            return new (int.Parse(g[1].Value), int.Parse(g[2].Value));
         }
 
-        for (long j = 0; j <= 10000000000000L; j += 10000000000000)
-        {
+        for (int j = 0; j < 2; j++)
             for (int i = 0; i < lines.Length; i += 3)
-            {
+            {   // Good ol' algebra
                 Vector2 a = Match(regexButton, lines[i]);
                 Vector2 b = Match(regexButton, lines[i + 1]);
-                Vector2 p = Match(new (@"Prize: X=(\d+), Y=(\d+)"), lines[i + 2]);
-                Console.WriteLine($"\nStarting machine {i + 1} ...");
-                Console.WriteLine($"A: X = {a.X}, Y = {a.Y}");
-                Console.WriteLine($"B: X = {b.X}, Y = {b.Y}");
-                Console.WriteLine($"P: X = {p.X}, Y = {p.Y}");
-                List<List<Vector2>> validCounts = [[], []];
-
-                for (int x = 0; x < (j == 0 ? 101 : j); x++)
-                {
-                    for (int y = 0; y < (j == 0 ? 101 : j); y++)
-                    {
-                        Vector2 a1 = new Vector2(a.X * x, a.Y * x);
-                        Vector2 b1 = new Vector2(b.X * y, b.Y * y);
-                        Vector2 solution = a1 + b1;
-                        if (solution.Length() > p.X + j + p.Y + j)
-                        {
-                            break;
-                        }
-
-                        if (solution.X == p.X + j && solution.Y == p.Y + j)
-                        {
-                            validCounts[j == 0 ? 0 : 1].Add(new Vector2(x, y));
-                        }
-                    }
-                    Console.Write($"\rProcessed: {(x + 1) * j} - Matches: {validCounts[j == 0 ? 0 : 1].Count}                    ");
-                }
-
-                Console.WriteLine("");
-
-                if (validCounts[j == 0 ? 0 : 1].Count == 0)
-                {
-                    Console.WriteLine("No combination possible!");
-                    continue;
-                }
-                long lowest = (long)validCounts[j == 0 ? 0 : 1].Select(x => x.X * 3 + x.Y).Order().First();
-                Console.WriteLine($"Lowest cost = {lowest}");
-                totals[j == 0 ? 0 : 1] += lowest;
-                
+                var pr = Match(new(@"Prize: X=(\d+), Y=(\d+)"), lines[i + 2]);
+                long[] p = [(long)pr.X, (long)pr.Y];
+                p[0] = j == 1 ? /*Part 2*/ p[0] + 10000000000000 : p[0]; 
+                p[1] = j == 1 ? /*Part 2*/ p[1] + 10000000000000 : p[1];
+                double y = (p[1] * (double)a.X - p[0] * (double)a.Y) 
+                    / (b.Y * (double)a.X - b.X * (double)a.Y);
+                double x = (p[0] - y * b.X) / a.X;
+                if (x % 1 != 0 || y % 1 != 0) continue; // Not an int
+                totals[j] += (long)x * 3 + (long)y;
             }
-            Console.WriteLine($"Part 1 solution = {totals[0]}");
-            Console.WriteLine($"Press enter key to continue");
-            Console.ReadLine();
-        }
     }
 }
