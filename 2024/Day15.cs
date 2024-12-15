236380eef84433bd179305c8b2fb82a2f7e75d9d
SplenectomY <skaterjohn16@hotmail.com>
2024-12-15 13:37:47 -0500

    Update Day15.cs
    Solve part 2

--- 2024/Day15.cs
+++ 2024/Day15.cs
@@ -12,110 +12,79 @@ namespace AdventOfCode._2024;
 /// </summary>
 [Day(2024, 15)]
 internal class Day15 : Day
-{
+{   //89
     public override async Task Solve(string input, long[] totals)
     {
         int p = 0;
         string[] lines = input.Split(Environment.NewLine);
-        Dictionary<char, Vector2> directions = new()
-        {
-            {'^', new(0, -1) },  {'>', new(1, 0) },
-            {'v', new(0, 1) },  {'<', new(-1,0) }
-        };
+        Dictionary<char, Vector2> directions = new() { {'^', new(0, -1) },  
+            {'>', new(1, 0) }, {'v', new(0, 1) },  {'<', new(-1,0) }};
         Start:
         Vector2 robot = Vector2.Zero;
-        Dictionary<Vector2, char> objects = [];
-        
-        // Parse map
+        Dictionary<List<Vector2>, char> obs = [];
         int y = 0;
+
         for (; y < lines.Length; y++)
         {
             if (string.IsNullOrEmpty(lines[y])) break;
             string line = p == 1 ? "" : lines[y];
-            if (p == 1) // Part 2
-            {
+            if (p > 0) // Part 2
                 for (int x = 0; x < lines[y].Length; x++)
-                {
-                    char c = lines[y][x];
-                    if (c == '#') line += "##";
-                    if (c == 'O') line += "[]";
-                    if (c == '.') line += "..";
-                    if (c == '@') line += "@.";
-                }
-            }
+                    if (lines[y][x] == '#') line += "##";
+                    else if (lines[y][x] == 'O') line += "[]";
+                    else if (lines[y][x] == '.') line += "..";
+                    else if (lines[y][x] == '@') line += "@.";
+
             for (int x = 0; x < line.Length; x++)
-            {
-                if (line[x] == '.') continue;
-                if (line[x] == '@')
-                {
-                    robot = new(x, y);
-                    continue;
-                }
-                objects[new Vector2(x, y)] = line[x];
-            }
+                if (line[x] == '[') obs[[new(x, y), new(x + 1, y)]] = 'O';
+                else if (line[x] == '@') robot = new(x, y);
+                else if (line[x] == 'O') obs[[new(x, y)]] = 'O';
+                else if (line[x] == '#') obs[[new(x, y)]] = '#';
         }
 
-        // ParseInstructions
-        for (++y; y < lines.Length; y++)
+        bool TryGetOccupied(Vector2 tile, out List<Vector2>? key)
         {
-            string line = lines[y];
-            for (int x = 0; x < line.Length; x++)
-            {
-                //Console.WriteLine($"Parsing robot direction {line[x]}");
-                var dir = directions[line[x]];
-                var newTile = robot + dir;
-
-                if (!objects.ContainsKey(newTile))
-                {
-                    robot = newTile;
-                    //Console.WriteLine("Moving robot");
-                    continue;
-                }
-
-                var obj = objects[newTile];
-
-                if (obj == '#')
-                {
-                    //Console.WriteLine("Hit wall");
-                    continue;
-                }
-
-                // Newtile is O
-                var nextObj = newTile;
-                List<Vector2> boxes = [];
-
-                while (true)
-                {
-                    boxes.Add(nextObj);
-                    nextObj += dir;
-                    if (!objects.ContainsKey(nextObj))
-                    {
-                        // Move all boxes over in that direction
-                        robot = newTile;
-
-                        foreach (var box in boxes)
-                            objects.Remove(box);
-
-                        boxes = boxes.Select(x => x + dir).ToList();
-
-                        for (int i = 0; i < boxes.Count; i++)
-                            objects[boxes[i]] = 'O';
-
-                        //Console.WriteLine("Moving box(es)");
-
-                        break;
-                    }
-                    else if (objects[nextObj] == '#') // wall
-                    {
-                        //Console.WriteLine("Hit wall");
-                        break;
-                    }
-                }
-            }
+            key = obs.Keys.FirstOrDefault(x => x.Contains(tile));
+            return key != null;
         }
 
-        totals[p++] = (long)objects.Where(x => x.Value == 'O').Select(x => x.Key.X + (100 * x.Key.Y)).Sum();
-        
-        goto Start;
+        bool TryMoveAllObjects(Vector2 dir, List<Vector2> obj, 
+            List<List<Vector2>> all)
+        {
+            all.Add(obj);
+            foreach (var item in obj)
+                if (TryGetOccupied(item + dir, out var objs))
+                    if (all.Contains(objs)) continue;
+                    else if (obs[objs] == '#') return false;
+                    else if (!TryMoveAllObjects(dir, objs, all)) return false;
+            return true;
+        }
+
+        for (++y; y < lines.Length; y++) // Iterate robot directions
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                var dir = directions[lines[y][x]];
+
+                if (!TryGetOccupied(robot + dir, out List<Vector2>? key))
+                {   // Nothing is in the way, move the robot
+                    robot += dir;
+                    continue;
+                }
+
+                if (obs[key] == '#') continue;
+                List<List<Vector2>> allObjs = [];
+                if (TryMoveAllObjects(dir, key, allObjs))
+                {
+                    robot += dir;
+                    foreach (var item in allObjs) obs.Remove(item);
+                    allObjs = [.. allObjs.Select(x => x
+                    .Select(y => y + dir).ToList())];
+                    foreach (var item in allObjs) obs[item] = 'O';
+                }
+            }
+
+        totals[p++] = (long)obs.Where(x => x.Value == 'O')
+            .Select(x => x.Key[0].X + (100 * x.Key[0].Y)).Sum();
+        if (p == 2) return; else goto Start;
     }
 }
