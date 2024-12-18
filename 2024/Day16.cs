c7ddbf06fffd63efc772b3b6206530e466cddd78
SplenectomY <skaterjohn16@hotmail.com>
2024-12-18 16:53:09 -0500

    Update Day16.cs
    Condense

--- 2024/Day16.cs
+++ 2024/Day16.cs
@@ -25,152 +25,73 @@ internal class Day16 : Day
         HashSet<Vector2> walls = [];
         List<Vector2> bestPath = [];
         List<Vector2> dirs = [new(0, 1), new(0, -1), new(1, 0), new(-1, 0)];
-        Vector2 ToEnd(Vector2 pos) => end - pos;
-        long totalMoves = 0;
-        HashSet<Vector2> allTiles = [];
-        List<Vector2> bestPathTiles = [];
-        List<List<Vector2>> bestPaths = [];
+        List<(long, List<Vector2>)> bestPaths = [];
         List<Action> actions1 = [];
-        Dictionary<Vector2, long> scoreAtTile = new Dictionary<Vector2, long>();
-        long totalKilled = 0;
+        Dictionary<Vector2, long> scoreAtTile = [];
 
         for (int y = 0; y < lines.Length; y++) 
-        { 
             for (int x = 0; x < lines[y].Length; x++)
             {
                 if (lines[y][x] == '#') walls.Add(new Vector2(x, y));
                 if (lines[y][x] == 'E') end = new Vector2(x, y);
                 if (lines[y][x] == 'S') start = new Vector2(x, y);
             }
-        }
-
-        IEnumerable<Vector2> GetSortedDirs(Vector2 pos) 
-        {
-            Vector2 toEnd = end - pos;
-            return dirs.OrderBy(x => Angle(toEnd, x));
-        }
-
-        double Angle(Vector2 v, Vector2 u) => Math.Acos(Vector2.Dot(v, u) 
-            / (v.Length() * u.Length()));
 
         void Path(Vector2 pos, List<Vector2> path, Vector2 dir, long score, int shuffle, Vector2 ending)
         {
-            totalMoves++;
-            //if (totalMoves % 100000 == 0) Draw();
-            if (score > lowestScore)
-            {
-                totalKilled++;
-                return;
-            }
+            if (score > lowestScore) return;
 
             //foreach (var direction in GetSortedDirs(pos))
             foreach (var direction in dirs.Skip(shuffle).Concat(dirs.Take(shuffle)))
             {
                 Vector2 next = pos + direction;
-                if (!walls.Contains(next))
+                if (walls.Contains(next)) continue;
+
+                Vector2 newDir = next - pos;
+                Vector2 combo = newDir + dir;
+
+                long newScore = combo == Vector2.Zero ? 2001 
+                    : (combo.X != 0 && combo.Y != 0) ? 1001 : 1;
+
+                if (newScore + score > lowestScore  
+                    || (scoreAtTile.TryGetValue(next, out long tileScore) 
+                    && newScore + score > tileScore))
+                    continue;
+
+                scoreAtTile[next] = newScore + score;
+
+                if (next == ending)
                 {
-                    //if (useAllTiles && allTiles.Contains(next)) continue;
-                    List<Vector2> newPath = [.. path, next];
-
-                    if (path.Last() == new Vector2(11, 3))
-                    {
-                        //Console.WriteLine(dir);
-                    }
-
-                    long newScore = GetScoreBetweenTiles(path.Last(), next, dir, out var newDir) + 1;
-
-                    if (newScore + score > lowestScore)
-                    {
-                        //Console.WriteLine("killed!");
-                        totalKilled++;
-                        continue;
-                    }
-
-                    if (scoreAtTile.TryGetValue(next, out long tileScore))
-                    {
-                        if (newScore + score > tileScore) continue;
-                    }
-
-                    scoreAtTile[next] = newScore + score;
-
-                    if (next == ending)
-                    {
-                        if (newScore + score <= lowestScore)
-                        {
-                            lowestScore = newScore + score;
-                            Console.Write($"\rLowest = {lowestScore}. Killed = {totalKilled}");
-                        }
-                        bestPaths.Add(newPath);
-                        continue;
-                    }
-
-                    Action action = () => Path(next, newPath, newDir, newScore + score, shuffle, ending);
-                    //Path(next, newPath, newDir, newScore + score, shuffle, ending);
-                    actions1.Add(action);
+                    lowestScore = Math.Min(lowestScore, newScore + score);
+                    bestPaths.Add((newScore + score, [.. path, next]));
+                    continue;
                 }
+
+                actions1.Add(() => Path(next, [.. path, next], newDir, newScore + score, shuffle, ending));
             }
         }
 
         void DoPath(Vector2 pos, List<Vector2> soFar, Vector2 dir, long score, Vector2 ending)
         {
-            for (int i = 0; i < 2; i++)
+            for (int y = 0; y < 4; y++)
             {
-                for (int y = 0; y < 4; y++)
+                scoreAtTile.Clear();
+                Path(pos, soFar, dir, score, y, ending);
+
+                while (actions1.Count > 0)
                 {
-                    scoreAtTile.Clear();
-                    Path(pos, soFar, dir, score, y, ending);
-
-                    while (actions1.Count > 0)
-                    {
-                        var todo = actions1.First();
-                        actions1.RemoveAt(0);
-                        todo.Invoke();
-                    }
+                    var todo = actions1.First();
+                    actions1.RemoveAt(0);
+                    todo.Invoke();
                 }
-                dirs.Reverse();
             }
-
         }
 
         DoPath(start, [start], new Vector2(1, 0), 0, end);
         DoPath(end, [end], new Vector2(1, 0), 0, start);
 
-        long GetScoreBetweenTiles(Vector2 current, Vector2 next, Vector2 dir, out Vector2 newDir)
-        {
-            long score = 0;
-            Vector2 pathDir = next - current;
-            Vector2 combo = pathDir + dir;
-
-            if (combo == Vector2.Zero)
-            {
-                score += 2000;
-            }
-            else if (combo.X != 0 && combo.Y != 0)
-            {
-                score += 1000;
-            }
-
-            newDir = pathDir;
-
-            return score;
-        }
-
-        long GetScore(List<Vector2> path, Vector2 dir)
-        {
-            long score = 0;
-            score = path.Count - 1;
-            for (int i = 0; i < path.Count - 1; i += 2)
-            {
-                Vector2 current = path[i];
-                Vector2 next = path[i + 1];
-                score += GetScoreBetweenTiles(current, next, dir, out dir);
-            }
-            
-            return score;
-        }
 
         totals[0] = lowestScore;
-        totals[1] = bestPaths.Where(x => GetScore(x, new(1, 0)) == lowestScore).SelectMany(x => x).Distinct().Count();
-        Console.WriteLine($"\nTotal moves: {totalMoves}");
+        totals[1] = bestPaths.Where(x => x.Item1 == lowestScore).SelectMany(x => x.Item2).Distinct().Count();
     }
 }
