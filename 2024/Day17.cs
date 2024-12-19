6ed6e8a10376e50de43b14401c0843017bb9a766
SplenectomY <skaterjohn16@hotmail.com>
2024-12-19 00:03:56 -0500

    Create Day17.cs

---
+++ 2024/Day17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2024;
+
+/// <summary>
+/// <see href="https://adventofcode.com/2024/day/17"/>
+/// </summary>
+[Day(2024, 17)]
+internal class Day17 : Day
+{
+    public override async Task Solve(string input, long[] totals)
+    {
+        
+    }
+}
