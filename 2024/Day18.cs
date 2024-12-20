7613b9faf0d07f9a8d39d59e1b3e295b0298baad
SplenectomY <skaterjohn16@hotmail.com>
2024-12-19 20:21:50 -0500

    Create Day18.cs

---
+++ 2024/Day18.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2024;
+
+/// <summary>
+/// <see href="https://adventofcode.com/2024/day/18"/>
+/// </summary>
+[Day(2024, 18)]
+internal class Day18 : Day
+{
+    public override async Task Solve(string input, dynamic[] totals)
+    {
+        int dims = Settings.IsTest ? 6 : 70;
+        List<Vector2> bytes = [];
+        Vector2 exit = new(dims, dims);
+        string[] lines = input.Split(Environment.NewLine);
+        foreach (var line in lines)
+        {
+            var g = new Regex(@"(\d+),(\d+)").Match(line).Groups;
+            bytes.Add(new(float.Parse(g[1].Value), float.Parse(g[2].Value)));
+        }
+
+        int max = Math.Min(bytes.Count, Settings.IsTest ? 12 : 1024);
+        bytes = bytes.Index().Where(x => x.Index < max).Select(x => x.Item).ToList();
+
+        List<HashSet<Vector2>> paths = [];
+
+        void Path(Vector2 pos, HashSet<Vector2> path)
+        {
+            
+        }
+    }
+}
