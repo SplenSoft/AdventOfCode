94cd7fea3bb07eb15d3dd14551ee81c97d4eb5f9
SplenectomY <skaterjohn16@hotmail.com>
2024-12-19 21:19:59 -0500

    Update Day18.cs

--- 2024/Day18.cs
+++ 2024/Day18.cs
@@ -17,7 +17,7 @@ internal class Day18 : Day
     public override async Task Solve(string input, dynamic[] totals)
     {
         int dims = Settings.IsTest ? 6 : 70;
-        List<Vector2> bytes = [];
+        HashSet<Vector2> bytes = [];
         Vector2 exit = new(dims, dims);
         string[] lines = input.Split(Environment.NewLine);
         foreach (var line in lines)
@@ -27,13 +27,75 @@ internal class Day18 : Day
         }
 
         int max = Math.Min(bytes.Count, Settings.IsTest ? 12 : 1024);
-        bytes = bytes.Index().Where(x => x.Index < max).Select(x => x.Item).ToList();
-
+        bytes = bytes.Index().Where(x => x.Index < max).Select(x => x.Item).ToHashSet();
+        List<Vector2> dirs = [new(0, 1), new(1, 0), new(-1, 0), new(0, -1)];
         List<HashSet<Vector2>> paths = [];
+        Dictionary<Vector2, int> tileScore = [];
+        double Angle(Vector2 v, Vector2 u) => Math.Acos(Vector2.Dot(v, u)
+            / (v.Length() * u.Length()));
 
+        IEnumerable<Vector2> GetDirs(Vector2 pos) => dirs.OrderBy(x => Math.Abs(Angle(exit - pos, x - pos)));
+
+        long lowest = long.MaxValue;
+        HashSet<Vector2> used = [];
+        long running = 0;
+        Path(new(0, 0), [new(0, 0)]);
+        
+        
         void Path(Vector2 pos, HashSet<Vector2> path)
         {
-            
+            running++;
+            if (running % 1000000 == 0)
+            {
+                Draw();
+            }
+            used.Add(pos);
+            if (pos == exit)
+            {
+                paths.Add(path);
+                if (path.Count - 1 < lowest)
+                {
+                    lowest = path.Count - 1;
+                    //Console.Write($"\rLowest: {lowest}                                           ");
+                }
+                return;
+            }
+
+            if (path.Count - 1 >= lowest) return;
+
+            if (!tileScore.TryGetValue(pos, out var score))
+                score = int.MaxValue;
+
+            if (path.Count > score) return;
+            tileScore[pos] = path.Count;
+
+            foreach (var dir in GetDirs(pos))
+            {
+                Vector2 next = pos + dir;
+                if (next.X < 0 || next.Y < 0 || next.X > dims || next.Y > dims) continue; // Stay on map
+                if (bytes.Contains(next)) continue;
+                if (path.Contains(next)) continue;
+                Path(next, [.. path, next]);
+            }
         }
+
+        void Draw()
+        {
+            Console.Clear();
+            for (int x = 0; x <= dims; x++)
+            {
+                string line = "";
+                for (int y = 0; y <= dims; y++)
+                {
+                    if (bytes.Contains(new Vector2(x, y))) line += '#';
+                    else if (used.Contains(new Vector2(x, y))) line += 'O';
+                    else line += ' ';
+                }
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+        }
+
+        totals[0] = lowest;
     }
 }
